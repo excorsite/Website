eaa8947edeabc2f29f67b26b4b748b55825239df
Excorsite <panditsuyog4@gmail.com>
2025-04-29 21:22:48 +0545

    props Types

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -18,6 +18,9 @@ Student.propTypes = {
 };
 
 export default Student;
+// props = read only properties that are shared between components.
+//       A parents component can send data to a child component.
+//       <Component key = value />
 
 // propTypes = a mechanism that ensures that the passed value is of the correct datatype.
 // age: propTypes.number
