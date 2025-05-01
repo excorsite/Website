097d754ac9a4f8d41717e6522cfe6b1537e02900
Excorsite <panditsuyog4@gmail.com>
2025-05-01 18:08:20 +0545

    render list

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,21 +1,26 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "banana", calories: 105 },
-    { id: 3, name: "orange", calories: 45 },
-    { id: 4, name: "mango", calories: 60 },
-    { id: 5, name: "pineapple", calories: 37 },
-    { id: 6, name: "Coconut", calories: 159 },
-  ];
+import PropTypes from "prop-types";
 
-  fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical Order
+function List(props) {
+  const categorys = props.category;
+  const itemsList = props.items;
+
+  //   const fruits = [
+  //     { id: 1, name: "apple", calories: 95 },
+  //     { id: 2, name: "banana", calories: 105 },
+  //     { id: 3, name: "orange", calories: 45 },
+  //     { id: 4, name: "mango", calories: 60 },
+  //     { id: 5, name: "pineapple", calories: 37 },
+  //     { id: 6, name: "Coconut", calories: 159 },
+  //   ];
+
+  // fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical Order
   // fruits.sort((a, b) => b.name.localeCompare(a.name)); // Reverse Alphabetical Order
   //  fruits.sort((a, b) => a.calories - b.calories);  // Numerical Order
   // fruits.sort((a, b) => b.calories - a.calories); //  Reverse Numerical Order
 
-  // const calFruits = fruits.filter((fruit) => fruit.calories < 100); // low calories fruit
+  const calFruits = itemsList.filter((fruit) => fruit.calories < 100); // low calories fruit
 
-  const calFruits = fruits.filter((fruit) => fruit.calories >= 100); // high calories fruit
+  // const calFruits = itemsList.filter((fruit) => fruit.calories >= 100); // high calories fruit
 
   const listItems1 = calFruits.map((lowcalFruit) => (
     <li key={lowcalFruit.id}>
@@ -23,7 +28,7 @@ function List() {
     </li>
   ));
 
-  const listItems = fruits.map((fruit) => (
+  const listItems = itemsList.map((fruit) => (
     <li key={fruit.id}>
       {fruit.name}: &nbsp; <b>{fruit.calories}</b>
     </li>
@@ -31,11 +36,29 @@ function List() {
 
   return (
     <>
-      <ul>{listItems}</ul>
-      <p>calories</p>
-      <ol>{listItems1}</ol>
+      <h3 className="list-category">{categorys}</h3>
+      <ol className="list-items">{listItems}</ol>
+      <p>
+        <h3 className="list-category">Low Calories</h3>
+      </p>
+      <ol className="list-items">{listItems1}</ol>
     </>
   );
 }
+List.prototype = {
+  category: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      calories: PropTypes.number,
+    })
+  ),
+};
+
+List.defaultProps = {
+  category: "Category",
+  items: [],
+};
 
 export default List;
--- my-react-app/src/Render.jsx
+++ my-react-app/src/Render.jsx
@@ -1,6 +1,30 @@
 import List from "./List";
 
 function Render() {
-  return <List />;
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "banana", calories: 105 },
+    { id: 3, name: "orange", calories: 45 },
+    { id: 4, name: "mango", calories: 60 },
+    { id: 5, name: "pineapple", calories: 37 },
+    { id: 6, name: "Coconut", calories: 159 },
+  ];
+
+  const vegetables = [
+    { id: 7, name: "potatoes", calories: 110 },
+    { id: 8, name: "celery", calories: 15 },
+    { id: 9, name: "carrots", calories: 25 },
+    { id: 10, name: "broccoli", calories: 50 },
+    { id: 11, name: "corn", calories: 63 },
+  ];
+
+  return (
+    <>
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {vegetables.length > 0 && (
+        <List items={vegetables} category="Vegetables" />
+      )}
+    </>
+  );
 }
 export default Render;
