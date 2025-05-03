80700a3a8fa2cf01aeee4e6a0488a1c062826e07
Excorsite <panditsuyog4@gmail.com>
2025-05-03 19:49:36 +0545

    click event

--- my-react-app/src/Button1.jsx
+++ my-react-app/src/Button1.jsx
@@ -1,8 +1,15 @@
 function Button1() {
-  //   const handleClick = () => console.log("Ouch!");
+  let count = 0;
 
-  const handleClick2 = (name) => console.log(" ${name} stop clicking me");
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++;
+      console.log(`${name} you clicked me ${count} time/s`);
+    } else {
+      console.log(`${name} stop clicking me!`);
+    }
+  };
 
-  return <button onClick={() => handleClick2("Suyog")}>Click Now ❤️</button>;
+  return <button onClick={() => handleClick("sathi")}>Click Now ❤️</button>;
 }
 export default Button1;
