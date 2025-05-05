aa93e8b620e44faf8c48844f200995660e683d75
Excorsite <panditsuyog4@gmail.com>
2025-05-05 18:57:28 +0545

    onChange Event

--- my-react-app/src/Change.jsx
+++ my-react-app/src/Change.jsx
@@ -7,6 +7,9 @@ import React, { useState } from "react";
 function Change() {
   const [name, setName] = useState("Guest");
   const [quantity, setQuantity] = useState(1);
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
 
   function handleQuantityChange(event) {
     setQuantity(event.target.value);
@@ -14,6 +17,18 @@ function Change() {
 
   function handleNameChange(event) {
     setName(event.target.value);
+  }
+
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
+
+  function handlePaymentChange(event) {
+    setPayment(event.target.value);
+  }
+
+  function handleShippingChange(event) {
+    setShipping(event.target.value);
   }
 
   return (
@@ -24,6 +39,41 @@ function Change() {
       <br />
       <input value={quantity} onChange={handleQuantityChange} type="number" />
       <p>Quantity: {quantity}</p>
+
+      <textarea
+        value={comment}
+        onChange={handleCommentChange}
+        placeholder="Enter a Delivery Message"
+      />
+      <p>Comment:{comment}</p>
+      <select value={payment} onChange={handlePaymentChange}>
+        <option value="">Select an Option</option>
+        <option value="Visa">Visa</option>
+        <option value="MasterCard">Master Card</option>
+        <option value="GiftCard">Gift Card</option>
+      </select>
+      <p>Payment: {payment}</p>
+
+      <label>
+        <input
+          type="radio"
+          value="Pick Up"
+          checked={shipping === "Pick Up"}
+          onChange={handleShippingChange}
+        />
+        Pick Up
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="Delivery"
+          checked={shipping === "Delivery"}
+          onChange={handleShippingChange}
+        />
+        Delivery
+      </label>
+      <p>Shipping: {shipping}</p>
     </div>
   );
 }
