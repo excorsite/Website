5f34de206343949f28a0768111772beda459fb27
Excorsite <panditsuyog4@gmail.com>
2025-05-14 20:30:31 +0545

    useRef() hook

--- my-react-app/src/UseRef/IsComponent.jsx
+++ my-react-app/src/UseRef/IsComponent.jsx
@@ -8,23 +8,51 @@
 // 2. Handling Focus, Animations, and Transitions
 // 3. Managing Timers and Intervals
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 function IsComponent() {
-  const inputRef = useRef(null);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
 
   useEffect(() => {
     console.log("COMPONENT RENDERING");
   });
 
-  function handleClick() {
-    inputRef.current.focus();
+  function handleClick1() {
+    inputRef1.current.focus();
+
+    inputRef1.current.style.backgroundColor = "yellow";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
+  }
+
+  function handleClick2() {
+    inputRef2.current.focus();
+
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "blue";
+    inputRef3.current.style.backgroundColor = "";
+  }
+
+  function handleClick3() {
+    inputRef3.current.focus();
+
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "red";
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Click me!</button>
-      <input ref={inputRef} />
+      <button onClick={handleClick1}>Click me1!</button>
+      <input ref={inputRef1} />
+
+      <button onClick={handleClick2}>Click me2!</button>
+      <input ref={inputRef2} />
+
+      <button onClick={handleClick3}>Click me3!</button>
+      <input ref={inputRef3} />
     </div>
   );
 }
